08129ddfa44dd58775c4d74063968601105199c7
Augustine Ugwu <run2austinfabian@gmail.com>
2024-01-04 04:32:51 +0100

    Austin forex academy

--- Austinfx.js
+++ Austinfx.js
@@ -58,6 +58,4 @@ $(document).ready(function () {
     loop: true,
   });
 
-  // for copy-right
-  $(".copyright").html(`<i class="far fa-copyright"></i> ${new Date().getFullYear()} All Right Reserved.`)
 });
---
+++ copyright.js
@@ -0,0 +1,2 @@
+// for copy-right
+$(".copyright").html(`<i class="far fa-copyright"></i> ${new Date().getFullYear()} All Right Reserved.`)
